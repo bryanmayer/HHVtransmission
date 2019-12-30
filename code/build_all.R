af2a4c1175ef2812656825d7d4f62621b3ce4877
Bryan <bmayer@fredhutch.org>
2019-12-30 15:44:42 -0800

    all updates after co-author review; edits to tables/figs; ID75

--- code/build_all.R
+++ code/build_all.R
@@ -1,2 +1,5 @@
-wflow_build(c("analysis/index.Rmd", "analysis/setup-exposure-data.Rmd",
-              "analysis/general-statistics.Rmd", "analysis/transmission-risk.Rmd"))
+# builds full pipeline
+wflow_build(c("analysis/about.Rmd", "analysis/index.Rmd",
+              "analysis/project-functions.Rmd", "analysis/setup-exposure-data.Rmd",
+              "analysis/general-statistics.Rmd",  "analysis/setup-model-data.Rmd",
+              "analysis/transmission-risk-sensitivity.Rmd", "analysis/transmission-risk.Rmd"))
