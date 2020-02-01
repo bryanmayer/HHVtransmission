448a776a214644e51a2e7f6cd9f65a2065bd8f30
Bryan <bmayer@fredhutch.org>
2020-02-01 10:42:27 -0800

    fixed formatting on api calls

--- code/build_all.R
+++ code/build_all.R
@@ -1,5 +1,13 @@
 # builds full pipeline
-wflow_build(c("analysis/about.Rmd", "analysis/index.Rmd",
-              "analysis/project-functions.Rmd", "analysis/setup-exposure-data.Rmd",
-              "analysis/general-statistics.Rmd",  "analysis/setup-model-data.Rmd",
-              "analysis/transmission-risk-sensitivity.Rmd", "analysis/transmission-risk.Rmd"))
+wflow_build(
+  c(
+    "analysis/about.Rmd",
+    "analysis/index.Rmd",
+    "analysis/project-functions.Rmd",
+    "analysis/setup-exposure-data.Rmd",
+    "analysis/general-statistics.Rmd",
+    "analysis/setup-model-data.Rmd",
+    "analysis/transmission-risk-sensitivity.Rmd",
+    "analysis/transmission-risk.Rmd"
+  )
+)
--- code/publish_all.R
+++ code/publish_all.R
@@ -1,5 +1,14 @@
 # publishes full pipeline
-wflow_publish(c("analysis/_site.yml", "analysis/about.Rmd", "analysis/index.Rmd",
-              "analysis/project-functions.Rmd", "analysis/setup-exposure-data.Rmd",
-              "analysis/general-statistics.Rmd",  "analysis/setup-model-data.Rmd",
-              "analysis/transmission-risk-sensitivity.Rmd", "analysis/transmission-risk.Rmd"))
+wflow_publish(
+  c(
+    "analysis/_site.yml",
+    "analysis/about.Rmd",
+    "analysis/index.Rmd",
+    "analysis/project-functions.Rmd",
+    "analysis/setup-exposure-data.Rmd",
+    "analysis/general-statistics.Rmd",
+    "analysis/setup-model-data.Rmd",
+    "analysis/transmission-risk-sensitivity.Rmd",
+    "analysis/transmission-risk.Rmd"
+  )
+)
